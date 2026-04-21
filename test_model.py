d03f26bd9bfb6e162f2c1358e8af6be266136325
Saurabh Singh <saurabh180387@gmail.com>
2026-04-21 09:23:39 +0530

    Test if model works

---
+++ test_model.py
@@ -0,0 +1,15 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# llm = ChatGoogleGenerativeAI(model="gemini-pro")
+
+# print(llm.invoke("Hello").content)
+
+import google.generativeai as genai
+import os
+
+genai.configure(api_key="AIzaSyDmGlaKcMhN2QWPpRTiUsVnNuyOJrRm1Pw")
+
+print("Available models:")
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
